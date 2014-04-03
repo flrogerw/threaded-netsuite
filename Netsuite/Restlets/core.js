48d51db7fa41727dae5c1c1da4f25ec797f6b256
Roger Williams <roger.williams@successories.com>
2014-04-03 10:07:49 -0400

    Contact creation error

--- Netsuite/Restlets/core.js
+++ Netsuite/Restlets/core.js
@@ -95,6 +95,33 @@ return( addressString.join('\n') )
   }
 
 
+	function currentCustomer( var customer ) {
+
+		var filters = [];
+		var columns = [];
+		var results = [];
+
+		
+		columns.push(new nlobjSearchColumn("custentity_customer_source_id"));
+		filters.push(new nlobjSearchFilter("custentity_customer_source_id",
+				null, "isnotempty", null));
+
+		var savedsearch = nlapiCreateSearch('customer', filters, columns);
+		var resultset = savedsearch.runSearch();
+		var searchid = 0;
+		do {
+			var resultslice = resultset.getResults(searchid, searchid + 1000);
+			for ( var rs in resultslice) {
+				results.push(resultslice[rs]);
+				searchid++;
+			}
+		} while (resultslice.length >= 1000);
+		return JSON.stringify(results);
+	}  
+  
+  
+  
+  
 function handleException(error) {
 	var message;
 	if (error instanceof nlobjError) {
