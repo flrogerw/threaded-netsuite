458d4074aa4dbbfaabf6db0326bc81d5f5a32953
Roger Williams <roger.williams@successories.com>
2014-04-18 10:11:04 -0400

    Updating Repository

--- Netsuite/Restlets/GC_Fulfillment_Check.js
+++ Netsuite/Restlets/GC_Fulfillment_Check.js
@@ -1,14 +1,59 @@
 /**
- * Checks for Giftcertificate if Wallet item in Sales Order
+ * Checks for Items Dependent on Other Items for Fulfillment
  * 
  * @author gWilli
- * 
+ * @date 04/2014
+ * @platform NetSuite 
  */
 
-var debug = true;
+/** 
+ * Items Required by Other Items 
+ */
+var dependentitems = [];
 
+/** 
+ * Items That Require Other Items 
+ */
+var itemsdependent = [];
+
+/**
+ * 
+ */
 var items = {
+		
+	/**
+	 * Loads all items with dependencies
+	 * 
+	 * @return void
+	 */ 
+	getdependentitems : function() {
 
+		if (dependentitems.length > 0) {
+			return;
+		}
+
+		var loadSearch = nlapiLoadSearch('item', 'customsearch_dependent_items');
+
+		var getData = loadSearch.runSearch();
+		getData.forEachResult(function(searchRow) {
+
+					itemsdependent.push(searchRow.getValue('name').replace(
+							/\s+/g, ''));
+					dependentitems.push(searchRow.getText(
+							'custitem_dependent_item').replace(/\s+/g, ''));
+
+					return true;
+				});
+	},
+	
+	/**
+	 * Adds new item to items object 
+	 * 
+	 * @param string itemname
+	 * @param int quantity
+	 * @param int internalid
+	 * @returns void
+	 */ 
 	additem : function(itemname, quantity, internalid) {
 
 		var itemKey = this.isitem(itemname);
@@ -20,16 +65,28 @@ var items = {
 		item.errors = [];
 	},
 
+	/**
+	 * Gets an Items dependent item from itemsdependent/dependentitems arrays
+	 * 
+	 * @param string itemname
+	 * @return mixed 
+	 */
 	getdependents : function(itemname) {
 
-		// Add logic here
-		if (itemname == 'GC25BlkWallet') {
-			return ("GC25");
+		if (itemsdependent.indexOf(itemname) >= 0) {
+
+			return (dependentitems[itemsdependent.indexOf(itemname)]);
 		} else {
 			return null;
 		}
 	},
 
+	/**
+	 * Test for exsistance of item in items object, creates new if not
+	 * 
+	 * @param string itemname
+	 * @returns string itemname
+	 */ 
 	isitem : function(itemname) {
 
 		itemname = itemname.replace(/\s+/g, '');
@@ -39,6 +96,12 @@ var items = {
 		return itemname;
 	},
 
+	/**
+	 * Creates new item object
+	 * 
+	 * @param string itemname
+	 * @returns void
+	 */
 	createitem : function(itemname) {
 
 		this[itemname] = {
@@ -46,6 +109,11 @@ var items = {
 		};
 	},
 
+	/**
+	 * Checks for Dependencies and Creates Item Object if Needed
+	 * 
+	 * @return void
+	 */
 	setdependents : function() {
 
 		for (i in items) {
@@ -60,35 +128,49 @@ var items = {
 		}
 	},
 
+	/**
+	 * Updates the Number of Items that Require Dependent Item
+	 * 
+	 * @return void
+	 */
 	updaterequiredby : function(itemname, quantity) {
 
 		var item = items[itemname];
 		item.requiredby = item.requiredby + quantity;
 	},
 
+	/**
+	 * Checks for Correct Amount of Ordered Compared to Required
+	 * 
+	 * @return void
+	 */
 	checkfulfillment : function() {
 		for (i in items) {
 
 			var item = items[i];
 			if (item.quantity < item.requiredby) {
-				item.errors.push(i + ' Requires ' + item.requiredby + ' only '
-						+ item.quantity + ' ordered');
+				item.errors.push(' ' + item.requiredby + ' Item(s) Require ' + i
+						+ ': ' + item.quantity + ' Were Ordered.');
 			}
-
 		}
 	},
 
+	/**
+	 * Checks Items Object for an Item(s) with Missing Dependencies
+	 * 
+	 * @return void
+	 */
 	haserrors : function() {
 
 		var errors = [];
 
-		for (i in items) {
+		for ( var i in items) {
 
 			var item = items[i];
 
 			if (typeof (item) !== "function") {
 				if (item.errors.length > 0) {
-					for (e in item.errors) {
+					for ( var e in item.errors) {
 						errors.push(item.errors[e]);
 					}
 				}
@@ -101,6 +183,11 @@ var items = {
 
 };
 
+/**
+ * Function Run Before Document Load
+ * 
+ * @param type
+ */
 function beforeLoad(type) {
 
 	if (nlapiGetContext().getExecutionContext() == "userinterface") {
@@ -116,6 +203,9 @@ function beforeLoad(type) {
 			// if more than zero
 			if (numofitems > 0) {
 
+				// load all items with dependencies
+				items.getdependentitems();
+
 				// spin through the sublist
 				for ( var i = 1; i <= numofitems; i++) {
 
@@ -124,22 +214,27 @@ function beforeLoad(type) {
 					var quantity = record.getLineItemValue('item', 'quantity',
 							i);
 					var internalid = record.getLineItemValue('item', 'item', i);
+					
+					// add item to items array
 					items.additem(itemname, quantity, internalid);
 
 				}
 			}
-
+			
+			// sets item dependencies
 			items.setdependents();
+			// check for correct number if dependencies
 			items.checkfulfillment();
+			
+			// check for fulfillment errors
 			var haserrors = items.haserrors();
 			if (haserrors !== false) {
-				throw haserrors.join();
+				throw haserrors;
 			}
-			nlapiLogExecution('DEBUG', 'Final Array', JSON.stringify(items));
+			//nlapiLogExecution('DEBUG', 'Final Array', JSON.stringify(items));
 
 		} catch (e) {
-
-			handleException(e);
+			throw e.join("<br>");
 		}
 	}
 }
