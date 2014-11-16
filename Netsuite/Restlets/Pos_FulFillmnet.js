f45c952e2d3523ee218750ee8597ef057a1c4a15
Roger Williams <roger.williams@successories.com>
2014-11-16 08:26:01 -0500

    Pushing to live

--- Netsuite/Restlets/Pos_FulFillmnet.js
+++ Netsuite/Restlets/Pos_FulFillmnet.js
@@ -1,48 +1,49 @@
-function main(){}
-
-function fulfillOrder( iOrderId ){
-	
-salesOrder = nlapiLoadRecord('salesorder',  iOrderId );
-//nlapiLogExecution('Debug', 'fulfillOrder', 'load status ' + salesOrder.getFieldValue('orderstatus'));
-
-var location = salesOrder.getFieldValue('location');
-
-if (location == '1') {
-	return;
+function main() {
 }
-var lineItemCount = salesOrder.getLineItemCount('item');
 
-// Start creating fulfillment
-var fulfillment = nlapiTransformRecord('salesorder', salesOrder.getId(),
-		'itemfulfillment');
+function fulfillOrder(iOrderId) {
 
-var bin, itemType;
-for ( var i = 1; i <= lineItemCount; i++) {
-	try {
+	salesOrder = nlapiLoadRecord('salesorder', iOrderId);
+	// nlapiLogExecution('Debug', 'fulfillOrder', 'load status ' +
+	// salesOrder.getFieldValue('orderstatus'));
 
-		itemLocation = salesOrder.getLineItemValue('item', 'location', i);
+	var location = salesOrder.getFieldValue('location');
 
-		// Unfulfill line items that do not match the header location
-		if (itemLocation != location) {
-			fulfillment.setLineItemValue('item', 'itemreceive', i, 'F');
-			continue;
-		}
-
-		itemType = fulfillment.getLineItemValue('item', 'itemtype', i);
-		
-		if (itemType != 'Kit') {
-			bin = getBinNumber(fulfillment.getLineItemValue('item', 'location',
-					i));
-			fulfillment.setLineItemValue('item', 'binnumbers', i, bin);
-		}
-
-	} catch (e) {
-		nlapiLogExecution('Error', 'onAfterSubmit', 'Error ' + e.message);
+	if (location == '1') {
+		return;
 	}
-}
+	var lineItemCount = salesOrder.getLineItemCount('item');
 
+	// Start creating fulfillment
+	var fulfillment = nlapiTransformRecord('salesorder', salesOrder.getId(),
+			'itemfulfillment');
 
-var fulfillmentId = nlapiSubmitRecord(fulfillment);
+	var bin, itemType;
+	for ( var i = 1; i <= lineItemCount; i++) {
+		try {
+
+			itemLocation = salesOrder.getLineItemValue('item', 'location', i);
+
+			// Unfulfill line items that do not match the header location
+			if (itemLocation != location) {
+				fulfillment.setLineItemValue('item', 'itemreceive', i, 'F');
+				continue;
+			}
+
+			itemType = fulfillment.getLineItemValue('item', 'itemtype', i);
+
+			if (itemType != 'Kit') {
+				bin = getBinNumber(fulfillment.getLineItemValue('item',
+						'location', i));
+				fulfillment.setLineItemValue('item', 'binnumbers', i, bin);
+			}
+
+		} catch (e) {
+			nlapiLogExecution('Error', 'onAfterSubmit', 'Error ' + e.message);
+		}
+	}
+
+	var fulfillmentId = nlapiSubmitRecord(fulfillment);
 }
 
 /*******************************************************************************
@@ -52,15 +53,15 @@ var fulfillmentId = nlapiSubmitRecord(fulfillment);
  ******************************************************************************/
 function getBinNumber(location) {
 
-switch(  parseInt( location )  ){
+	switch (parseInt(location)) {
 
-case(7):
-         return 'BocaTC';
-break;
-case(11):
-       return 'Culver';
-break;
+	case (7):
+		return 'BocaTC';
+		break;
+	case (11):
+		return 'Culver';
+		break;
 
-}
-	
+	}
+
 }
